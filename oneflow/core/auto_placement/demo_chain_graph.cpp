0d1875d8c9e06fe1c73d210c485d0e98b55dc998
Xinqi Li <lixinqi0703106@163.com>
2018-02-24 22:28:40 +0800

    complete code of DemoChainGraph
    Former-commit-id: a6a5ceea0347a8689d6037913ece4c1b9c22b3e0

--- oneflow/core/auto_placement/demo_chain_graph.cpp
+++ oneflow/core/auto_placement/demo_chain_graph.cpp
@@ -79,12 +79,204 @@ void DemoChainGraph::Consume(DemoChainNode* node, DemoChainRegst* regst) {
   Connect(regst->mut_producer(), NewEdge(), node);
 }
 
-void DemoChainGraph::Backward(DemoChainRegst* regst) {
+void DemoChainGraph::Loss(DemoChainRegst* regst) {
   regst->HandleDiff(NewRegst(regst->mut_producer()));
 }
 
-void DemoChainGraph::MakeGraph() {
-  Backward(Op("soft_max", {Op("fc", {Op("feature")}), Op("label")}));
+void DemoChainGraph::LogicalGraph() {
+  Loss(Op("soft_max", {Op("fc", {Op("feature")}), Op("label")}));
+}
+
+size_t DemoChainGraph::FwChainNodeNum() const {
+  size_t num = 0;
+  ForEachNode([&](const DemoChainNode* node) {
+    num += (node->chain_node_id() == node->fw_chain_node_id() ? 1 : 0);
+  });
+  return num;
+}
+
+size_t DemoChainGraph::ChainNodeNum() const {
+  size_t num = 0;
+  ForEachNode([&](const DemoChainNode* node) { ++num; });
+  return num;
+}
+
+DemoChainGraph::DemoChainGraph() : chain_node_id_(-1), chain_regst_id_(-1) {
+  LogicalGraph();
+  InitIsReachable();
+  InitRegst2ChainNodeSubGraphs();
+}
+
+IsReachablePredicator DemoChainGraph::MakeIsReachablePredicator() const {
+  using NodeRelation =
+      HashMap<const DemoChainNode*, std::unordered_set<const DemoChainNode*>>;
+  std::shared_ptr<NodeRelation> node2ancestors(new NodeRelation());
+  TopoForEachChainNode([&](const DemoChainNode* node) {
+    ForEachInNode(node, [&](const DemoChainNode* prev) {
+      (*node2ancestors)[node].insert((*node2ancestors)[prev].begin(),
+                                     (*node2ancestors)[prev].end());
+      (*node2ancestors)[node].insert(prev);
+    });
+  });
+  return [=](const DemoChainNode* src, const DemoChainNode* dst) {
+    const auto& it = node2ancestors->find(dst);
+    if (it == node2ancestors->end()) { return false; }
+    return it->second.find(src) != it->second.end();
+  };
+}
+
+void DemoChainGraph::TopoForEachChainNode(
+    const std::function<void(const DemoChainNode*)>& Handler) const {
+  std::list<const DemoChainNode*> sources;
+  ForEachNode([&](const DemoChainNode* node) {
+    int64_t in_cnt = 0;
+    ForEachInNode(node, [&](const DemoChainNode*) { ++in_cnt; });
+    if (in_cnt == 0) { sources.push_back(node); }
+  });
+
+  auto ForEachIn = std::bind(&DemoChainGraph::ForEachInNode, this,
+                             std::placeholders::_1, std::placeholders::_2);
+  auto ForEachOut = std::bind(&DemoChainGraph::ForEachOutNode, this,
+                              std::placeholders::_1, std::placeholders::_2);
+  using NodeVisitor = GraphNodeVisitorUtil<const DemoChainNode*>;
+  NodeVisitor::TopoForEach(sources, ForEachIn, ForEachOut, Handler);
+}
+
+void DemoChainGraph::ForEachInNode(
+    const DemoChainNode* node,
+    const std::function<void(const DemoChainNode*)>& Handler) const {
+  node->ForEachNodeOnInEdge([&](const DemoChainNode* in_node) {
+    if (in_node->task_type() == TaskType::kMdDiffAcc) { return; }
+    Handler(in_node);
+  });
+}
+
+void DemoChainGraph::ForEachOutNode(
+    const DemoChainNode* node,
+    const std::function<void(const DemoChainNode*)>& Handler) const {
+  if (node->task_type() == TaskType::kMdDiffAcc) { return; }
+  node->ForEachNodeOnOutEdge(Handler);
+}
+
+void DemoChainGraph::InitIsReachable() {
+  is_reachable_ = MakeIsReachablePredicator();
+}
+
+void DemoChainGraph::InitRegst2ChainNodeSubGraphs() {
+  for (const auto& regst : regsts_) {
+    auto sub_graph = of_make_unique<DemoChainNodeSubGraph>(
+        regst->producer(), regst->consumers(), is_reachable_);
+    regst2chain_node_sub_graph_.emplace(regst.get(), std::move(sub_graph));
+  }
+}
+
+std::vector<std::vector<int64_t>>
+DemoChainGraph::CalcChainNodeId2FwChainNodeId() const {
+  std::vector<std::vector<int64_t>> ret(ChainNodeNum());
+  ForEachNode([&](const DemoChainNode* node) {
+    ret.at(node->chain_node_id()).push_back(node->fw_chain_node_id());
+  });
+  return ret;
+}
+
+std::vector<std::vector<int64_t>>
+DemoChainGraph::CalcChainRegstId2ProducerChainNodeId() const {
+  std::vector<std::vector<int64_t>> ret(regsts_.size());
+  for (const auto& regst : regsts_) {
+    int64_t chain_node_id = regst->producer()->chain_node_id();
+    ret.at(regst->chain_regst_id()).push_back(chain_node_id);
+  }
+  return ret;
+}
+
+void DemoChainNodeSubGraph::TopoForEachChainNode(
+    const std::function<void(const DemoChainNode*)>& Handler) const {
+  auto ForEachIn = std::bind(&DemoChainNodeSubGraph::ForEachInNode, this,
+                             std::placeholders::_1, std::placeholders::_2);
+  auto ForEachOut = std::bind(&DemoChainNodeSubGraph::ForEachOutNode, this,
+                              std::placeholders::_1, std::placeholders::_2);
+  using NodeVisitor = GraphNodeVisitorUtil<const DemoChainNode*>;
+  NodeVisitor::TopoForEach({start_node_}, ForEachIn, ForEachOut, Handler);
+}
+
+void DemoChainNodeSubGraph::ForEachInNode(
+    const DemoChainNode* node,
+    const std::function<void(const DemoChainNode*)>& Handler) const {
+  if (node->task_type() == TaskType::kMdUpdt) {
+    node->ForEachNodeOnInEdge(Handler);
+  } else {
+    node->ForEachNodeOnInEdge([&](const DemoChainNode* in_node) {
+      if (IsReachableFromStartNode(in_node)) { Handler(in_node); }
+    });
+  }
+}
+
+void DemoChainNodeSubGraph::ForEachOutNode(
+    const DemoChainNode* node,
+    const std::function<void(const DemoChainNode*)>& Handler) const {
+  if (node->task_type() == TaskType::kMdDiffAcc) {
+    node->ForEachNodeOnOutEdge(Handler);
+  } else {
+    node->ForEachNodeOnOutEdge([&](const DemoChainNode* out_node) {
+      if (IsReachableToEndNode(out_node)) { Handler(out_node); }
+    });
+  }
+}
+
+bool DemoChainNodeSubGraph::IsReachableFromStartNode(
+    const DemoChainNode* node) const {
+  return node == start_node_ || IsReachable(start_node_, node);
+}
+
+bool DemoChainNodeSubGraph::IsReachableToEndNode(
+    const DemoChainNode* node) const {
+  for (const auto* end_node : end_nodes_) {
+    if (node == end_node || IsReachable(node, end_node)) { return true; }
+  }
+  return false;
+}
+
+void DemoChainNodeSubGraph::CalcLongestPath(
+    std::vector<int64_t>* path,
+    const std::function<double(int64_t)>& Time4ChainNodeId) const {
+  HashMap<const DemoChainNode*, double> node2longest_path_time;
+  HashMap<const DemoChainNode*, std::vector<int64_t>> node2path;
+  TopoForEachChainNode([&](const DemoChainNode* node) {
+    double time = 0;
+    const DemoChainNode* longest_path_tail_node = nullptr;
+    ForEachInNode(node, [&](const DemoChainNode* in_node) {
+      double t = node2longest_path_time[in_node];
+      if (time < t) {
+        time = t;
+        longest_path_tail_node = in_node;
+      }
+    });
+    node2longest_path_time[node] =
+        time + Time4ChainNodeId(node->chain_node_id());
+    node2path[node] = node2path[longest_path_tail_node];
+    node2path.at(node).push_back(node->chain_node_id());
+  });
+  double time = 0;
+  const DemoChainNode* longest_path_tail_node = nullptr;
+  for (const DemoChainNode* node : end_nodes_) {
+    double t = node2longest_path_time.at(node);
+    if (time < t) {
+      time = t;
+      longest_path_tail_node = node;
+    }
+  }
+  *path = node2path.at(longest_path_tail_node);
+}
+
+std::vector<std::vector<int64_t>>
+DemoChainGraph::CalcChainRegstId2PathChainNodeIds(
+    const std::function<double(int64_t)>& Time4ChainNodeId) const {
+  std::vector<std::vector<int64_t>> ret(regsts_.size());
+  for (const auto& pair : regst2chain_node_sub_graph_) {
+    int64_t chain_regst_id = pair.first->chain_regst_id();
+    pair.second->CalcLongestPath(&ret.at(chain_regst_id), Time4ChainNodeId);
+  }
+  return ret;
 }
 
 }  // namespace df
--- oneflow/core/auto_placement/demo_chain_graph.h
+++ oneflow/core/auto_placement/demo_chain_graph.h
@@ -2,12 +2,18 @@
 #define ONEFLOW_CORE_AUTO_PLACEMENT_DEMO_CHAIN_GRAPH_H_
 #include "oneflow/core/graph/graph.h"
 #include "oneflow/core/job/task.pb.h"
+#include "oneflow/core/graph/graph_node_visitor_util.h"
 
 namespace oneflow {
 
 namespace df {
 
 class DemoChainNode;
+using ChainNodeVisitor = GraphNodeVisitorUtil<const DemoChainNode*>;
+using ChainNodeHandler =
+    GraphNodeVisitorUtil<const DemoChainNode*>::HandlerType;
+using IsReachablePredicator =
+    std::function<bool(const DemoChainNode* src, const DemoChainNode* dst)>;
 
 class DemoChainRegst final {
  public:
@@ -16,24 +22,30 @@ class DemoChainRegst final {
       : producer_(producer), chain_regst_id_(chain_regst_id) {}
   ~DemoChainRegst() = default;
 
-  const DemoChainNode* producer() const { return producer_; }
-  DemoChainNode* mut_producer() { return producer_; }
-
   void HandleDiff(DemoChainRegst* regst) const { diff_handler_(regst); }
+
+  // Getters
+  int64_t chain_regst_id() const { return chain_regst_id_; }
+  const DemoChainNode* producer() const { return producer_; }
+  const std::unordered_set<const DemoChainNode*>& consumers() const {
+    return consumers_;
+  }
+
+  // Setters
+  DemoChainNode* mut_producer() { return producer_; }
   void set_diff_handler(
       const std::function<void(DemoChainRegst*)>& diff_handler) {
     diff_handler_ = diff_handler;
   }
-
   void AddConsumer(const DemoChainNode* consumer) {
-    consumers_.push_back(consumer);
+    consumers_.insert(consumer);
   }
 
  private:
   DemoChainNode* producer_;
   int64_t chain_regst_id_;
   std::function<void(DemoChainRegst*)> diff_handler_;
-  std::list<const DemoChainNode*> consumers_;
+  std::unordered_set<const DemoChainNode*> consumers_;
 };
 
 class DemoChainEdge;
@@ -46,6 +58,7 @@ class DemoChainNode final : public Node<DemoChainNode, DemoChainEdge> {
   ~DemoChainNode() = default;
 
   const std::string& name() const { return name_; }
+  TaskType task_type() const { return task_type_; }
   int64_t chain_node_id() const { return chain_node_id_; }
   int64_t fw_chain_node_id() const { return fw_chain_node_id_; }
 
@@ -64,6 +77,41 @@ class DemoChainNode final : public Node<DemoChainNode, DemoChainEdge> {
   int64_t fw_chain_node_id_;
 };
 
+class DemoChainNodeSubGraph final {
+ public:
+  OF_DISALLOW_COPY_AND_MOVE(DemoChainNodeSubGraph);
+  DemoChainNodeSubGraph(
+      const DemoChainNode* start_node,
+      const std::unordered_set<const DemoChainNode*>& end_nodes,
+      const IsReachablePredicator& is_reachable)
+      : start_node_(start_node),
+        end_nodes_(end_nodes),
+        is_reachable_(&is_reachable) {}
+  ~DemoChainNodeSubGraph() = default;
+  void CalcLongestPath(
+      std::vector<int64_t>* path,
+      const std::function<double(int64_t)>& Time4ChainNodeId) const;
+
+ private:
+  void TopoForEachChainNode(
+      const std::function<void(const DemoChainNode*)>& Handler) const;
+  void ForEachInNode(
+      const DemoChainNode* node,
+      const std::function<void(const DemoChainNode*)>& Handler) const;
+  void ForEachOutNode(
+      const DemoChainNode* node,
+      const std::function<void(const DemoChainNode*)>& Handler) const;
+  bool IsReachable(const DemoChainNode* src, const DemoChainNode* dst) const {
+    return (*is_reachable_)(src, dst);
+  }
+  bool IsReachableFromStartNode(const DemoChainNode* node) const;
+  bool IsReachableToEndNode(const DemoChainNode* node) const;
+
+  const DemoChainNode* start_node_;
+  std::unordered_set<const DemoChainNode*> end_nodes_;
+  const IsReachablePredicator* is_reachable_;
+};
+
 class DemoChainEdge final : public Edge<DemoChainNode, DemoChainEdge> {
  public:
   OF_DISALLOW_COPY_AND_MOVE(DemoChainEdge);
@@ -74,9 +122,25 @@ class DemoChainEdge final : public Edge<DemoChainNode, DemoChainEdge> {
 class DemoChainGraph : public Graph<DemoChainNode, DemoChainEdge> {
  public:
   OF_DISALLOW_COPY_AND_MOVE(DemoChainGraph);
-  DemoChainGraph() : chain_node_id_(-1), chain_regst_id_(-1) {}
+  DemoChainGraph();
   virtual ~DemoChainGraph() = default;
-  virtual void MakeGraph();
+  virtual void LogicalGraph();
+
+  size_t FwChainNodeNum() const;
+  size_t ChainNodeNum() const;
+
+  std::vector<std::vector<int64_t>> CalcChainNodeId2FwChainNodeId() const;
+
+  std::vector<std::vector<int64_t>> CalcChainRegstId2ProducerChainNodeId()
+      const;
+
+  std::vector<std::vector<int64_t>> CalcChainRegstId2PathChainNodeIds(
+      const std::function<double(int64_t)>& GetTime) const;
+
+  std::vector<std::vector<int64_t>> CalcChainRegstId2PathChainNodeIds() const {
+    return CalcChainRegstId2PathChainNodeIds(
+        [](int64_t) -> double { return 1; });
+  }
 
  protected:
   DemoChainRegst* Op(const std::string& name,
@@ -84,11 +148,22 @@ class DemoChainGraph : public Graph<DemoChainNode, DemoChainEdge> {
 
   DemoChainRegst* Op(const std::string& name) { return Op(name, {}); }
   DemoChainRegst* Model(const std::string& name);
-  void Backward(DemoChainRegst* regst);
+  void Loss(DemoChainRegst* regst);
 
  private:
   int64_t NewChainNodeId() { return ++chain_node_id_; }
   int64_t NewChainRegstId() { return ++chain_node_id_; }
+  void InitIsReachable();
+  void InitRegst2ChainNodeSubGraphs();
+  IsReachablePredicator MakeIsReachablePredicator() const;
+  void TopoForEachChainNode(
+      const std::function<void(const DemoChainNode*)>& Handler) const;
+  void ForEachInNode(
+      const DemoChainNode* node,
+      const std::function<void(const DemoChainNode*)>& Handler) const;
+  void ForEachOutNode(
+      const DemoChainNode* node,
+      const std::function<void(const DemoChainNode*)>& Handler) const;
   DemoChainRegst* NewRegst(DemoChainNode* producer);
   DemoChainNode* NewChainNode(const std::string& name, TaskType task_type);
   DemoChainNode* NewForwardNode(const std::string& name);
@@ -100,6 +175,9 @@ class DemoChainGraph : public Graph<DemoChainNode, DemoChainEdge> {
   int64_t chain_node_id_;
   int64_t chain_regst_id_;
   std::list<std::unique_ptr<DemoChainRegst>> regsts_;
+  IsReachablePredicator is_reachable_;
+  HashMap<const DemoChainRegst*, std::unique_ptr<DemoChainNodeSubGraph>>
+      regst2chain_node_sub_graph_;
 };
 
 }  // namespace df
