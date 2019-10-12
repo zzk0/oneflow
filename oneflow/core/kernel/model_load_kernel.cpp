b97e0c08a6b21baa581d40269b97c5b1b102a9c0
guo-ran <360112263@qq.com>
2019-10-12 14:05:58 +0800

    model load normalize
    Former-commit-id: 1544e8bcc57d81cda1e5ce3d5c90b9971aa58886

--- oneflow/core/kernel/model_load_kernel.cpp
+++ oneflow/core/kernel/model_load_kernel.cpp
@@ -1,4 +1,5 @@
 #include "oneflow/core/kernel/kernel.h"
+#include "oneflow/core/kernel/normalize_kernel_util.h"
 
 namespace oneflow {
 
@@ -21,6 +22,11 @@ class ModelLoadKernel final : public KernelIf<DeviceType::kCPU> {
       const std::string key =
           GenLogicalBlobName(conf.variable_op_name(i), original_variable_conf.out());
       reader.Read(key, out_i);
+      if (original_variable_conf.has_normalize_conf()) {
+        NormalizeKernelUtil<DeviceType::kCPU, float>::Normalize(
+          ctx.device_ctx, original_variable_conf.normalize_conf().axis(), original_variable_conf.normalize_conf().epsilon(),
+          out_i, BnInOp2Blob("square_x_sum"), out_i);
+      }
     }
   }
 };
