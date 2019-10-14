466a8265c88eef682dcf3449f8c113367c7e36ec
guo-ran <360112263@qq.com>
2019-10-14 21:48:53 +0800

    exp op
    Former-commit-id: 7f93a18541acdd3c6acdbad62cbd414f855f0ad6

---
+++ oneflow/core/kernel/exp_kernel.cpp
@@ -0,0 +1,17 @@
+#include "oneflow/core/kernel/exp_kernel.h"
+#include "oneflow/core/kernel/new_kernel_util.h"
+
+namespace oneflow {
+
+template<DeviceType device_type, typename T>
+void ExpKernel<device_type, T>::ForwardDataContent(
+    const KernelCtx& ctx, std::function<Blob*(const std::string&)> BnInOp2Blob) const {
+  Blob* in_blob = BnInOp2Blob("in");
+  NewKernelUtil<device_type>::Exp(ctx.device_ctx, in_blob->shape().elem_cnt(), in_blob->dptr<T>(),
+                                   BnInOp2Blob("out")->mut_dptr<T>());
+}
+
+ADD_DEFAULT_KERNEL_CREATOR_WITH_GPU_HALF(OperatorConf::kExpConf, ExpKernel,
+                                         FLOATING_DATA_TYPE_SEQ);
+
+}  // namespace oneflow
