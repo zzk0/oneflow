51acf56e9b4bc1dedd8a64be8b9cc361ec3faf52
guo-ran <360112263@qq.com>
2019-10-17 19:06:06 +0800

    fix arc_face_grad_kernel
    Former-commit-id: cca0310320a8589aea3017998971257cd4b14457

--- oneflow/core/kernel/arc_face_grad_kernel.cpp
+++ oneflow/core/kernel/arc_face_grad_kernel.cpp
@@ -13,7 +13,7 @@ template<DeviceType device_type, typename T>
 void ArcFaceGradKernel<device_type, T>::ForwardDataContent(
     const KernelCtx& ctx, std::function<Blob*(const std::string&)> BnInOp2Blob) const {
   const float margin = this->op_conf().arc_face_grad_conf().margin();
-  const int64_t lower_bound = this->kernel_conf().arc_face_conf().lower_bound();
+  const int64_t lower_bound = this->kernel_conf().arc_face_grad_conf().lower_bound();
   const T cos_m = cos(margin);
   const T sin_m = sqrt(1 - cos_m * cos_m);
   BnInOp2Blob("dx")->CopyDataContentFrom(ctx.device_ctx, BnInOp2Blob("dy"));
