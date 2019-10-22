d715ab73809fadaffd83a5985926f3c06e29b134
guo-ran <360112263@qq.com>
2019-10-22 14:02:50 +0800

    variable set lr and model update normalize
    Former-commit-id: 7bd805eae60185bdd689279ca23cc5e6596ec235

--- oneflow/core/job_completer/optimizer.cpp
+++ oneflow/core/job_completer/optimizer.cpp
@@ -1,6 +1,45 @@
 #include "oneflow/core/job_completer/optimizer.h"
+#include "oneflow/core/graph/op_graph.h"
+#include "oneflow/core/job/job.pb.h"
 
 namespace oneflow {
+
+namespace {
+
+std::string AddScheduleOp(JobBuilder* job_builder, const TrainConf& train_conf,
+                          const std::string& op_name, const float learning_rate) {
+  const ParallelConf& parallel_conf =
+      job_builder->ParallelConf4OpName(GenLogicalBlobId(train_conf.train_step_lbn()).op_name());
+  const NormalModelUpdateOpUserConf& model_update_conf = train_conf.model_update_conf();
+  if (model_update_conf.has_warmup_conf() || model_update_conf.has_learning_rate_decay()) {
+    OperatorConf schedule_op_conf{};
+    schedule_op_conf.set_name(op_name);
+    LearningRateScheduleOpConf* schedule_conf =
+        schedule_op_conf.mutable_learning_rate_schedule_conf();
+    schedule_conf->set_train_step(train_conf.train_step_lbn());
+    schedule_conf->set_learning_rate(learning_rate);
+    schedule_conf->set_out("out");
+    if (model_update_conf.has_warmup_conf()) {
+      *schedule_conf->mutable_warmup_conf() = model_update_conf.warmup_conf();
+    }
+    if (model_update_conf.has_learning_rate_decay()) {
+      *schedule_conf->mutable_learning_rate_decay() = model_update_conf.learning_rate_decay();
+    }
+    job_builder->AddOps(parallel_conf, {schedule_op_conf});
+    return GenLogicalBlobName(op_name, schedule_conf->out());
+  } else {
+    OperatorConf constant_op_conf{};
+    constant_op_conf.set_name(op_name);
+    ConstantOpConf* constant_conf = constant_op_conf.mutable_constant_conf();
+    constant_conf->set_out("out");
+    *constant_conf->mutable_shape()->mutable_dim()->Add() = 1;
+    constant_conf->set_data_type(DataType::kFloat);
+    constant_conf->mutable_initializer()->mutable_constant_conf()->set_value(learning_rate);
+    job_builder->AddOps(parallel_conf, {constant_op_conf});
+    return GenLogicalBlobName(op_name, constant_conf->out());
+  }
+}
+}  // namespace
 
 void GenerateOptimizerOpConfWrapperStruct::Call(
     const VariableOp& var_op, const ParallelConf& parallel_conf, JobBuilder* job_builder,
@@ -63,8 +102,21 @@ void ConstructMdUpdtOpConf(const VariableOp& op, const LogicalBlobId& diff_lbi_o
   mdupdt_op_conf->set_total_instance_num_diff(GenLogicalBlobName(total_loss_instance_num_lbi));
   mdupdt_op_conf->set_model(GenLogicalBlobName(op.BnInOp2Lbi("out")));
   mdupdt_op_conf->set_train_step(train_conf.train_step_lbn());
-  const std::string& primary_lr_lbn = train_conf.primary_lr_lbn();
+  const std::string& primary_lr_lbn =
+      op.op_conf().variable_conf().has_primary_lr()
+          ? AddScheduleOp(job_builder, train_conf,
+                          "Variable-Train-PrimaryLearningRate-Scheduler" + op.op_conf().name(),
+                          op.op_conf().variable_conf().primary_lr())
+          : train_conf.primary_lr_lbn();
   const std::string& secondary_lr_lbn = train_conf.secondary_lr_lbn();
+
+  if (op.op_conf().variable_conf().has_normalize_conf()) {
+    mdupdt_op_conf->mutable_normalize_conf()->set_epsilon(
+        op.op_conf().variable_conf().normalize_conf().epsilon());
+    mdupdt_op_conf->mutable_normalize_conf()->set_axis(
+        op.op_conf().variable_conf().normalize_conf().axis());
+  }
+
   if (op.op_conf().variable_conf().model_name() == "weight") {
     mdupdt_op_conf->set_learning_rate(primary_lr_lbn);
     mdupdt_op_conf->set_l1(train_conf.weight_l1());
