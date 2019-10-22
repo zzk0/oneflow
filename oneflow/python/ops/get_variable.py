4a717ba021d33abbfb79e04a63e7b4310dfbaf9f
guo-ran <360112263@qq.com>
2019-10-22 17:52:23 +0800

    get_variable: add primary_lr & normalize
    Former-commit-id: 15e7c5fe904bd79aeb84f2c9272e957cbae2016f

--- oneflow/python/ops/get_variable.py
+++ oneflow/python/ops/get_variable.py
@@ -18,6 +18,10 @@ def get_variable(
     trainable=None,
     model_name=None,
     random_seed=None,
+    primary_lr=None,
+    normalize=None,
+    norm_axis=None,
+    norm_epsilon=None,
     distribute=distribute_util.broadcast(),
 ):
     assert isinstance(name, str)
@@ -44,6 +48,12 @@ def get_variable(
             op_conf.variable_conf.split_axis.value = distribute.axis
         else:
             op_conf.variable_conf.split_axis.ClearField("value")
+        if primary_lr is not None:
+            op_conf.variable_conf.primary_lr = primary_lr
+        if normalize is not None:
+            if normalize:
+                op_conf.variable_conf.normalize_conf.axis = norm_axis
+                op_conf.variable_conf.normalize_conf.epsilon = norm_epsilon
 
         if random_seed is not None:
             op_conf.variable_conf.random_seed = random_seed
