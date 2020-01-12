4d67ebcd582578c126dd5bad131fc244431922db
lixinqi <lixinqi0703106@163.com>
2020-01-12 16:39:09 +0800

    backup code
    Former-commit-id: 475f2ed4850698005aecf0ec17b6817de956c2b2

--- oneflow/core/blob_curd/blob_curd.h
+++ oneflow/core/blob_curd/blob_curd.h
@@ -2,25 +2,17 @@
 #define ONEFLOW_CORE_BLOB_CURD_BLOB_CURD_H_
 
 #include "oneflow/core/common/util.h"
-#include "oneflow/core/common/preprocessor.h"
+#include "oneflow/core/common/pod_proto.h"
 
 namespace oneflow {
 
 #define BLOB_CURD_METHOD_SEQ \
+  OF_PP_MAKE_TUPLE_SEQ(invalide_curd) \
   OF_PP_MAKE_TUPLE_SEQ(fill_last_tensor)
 
 #define BLOB_CURD_TYPE(method) k_##method##_BlobCurdType
 
-#define DEFINE_FIELD_AND_ACCESSOR(type, field_name)        \
- public:                                                   \
-  type field_name() const { return field_name##_; }        \
-  void set_##field_name(type val) { field_name##_ = val; } \
-                                                           \
- private:                                                  \
-  type field_name##_;
-
 enum BlobCurdType {
-  kInvalidBlobCurdType = 0,
 #define MAKE_BLOB_CURD_TYPE(method) BLOB_CURD_TYPE(method),
   OF_PP_FOR_EACH_TUPLE(MAKE_BLOB_CURD_TYPE, BLOB_CURD_METHOD_SEQ)
 #undef MAKE_BLOB_CURD_TYPE
@@ -34,42 +26,25 @@ struct BlobCurdResponseParam {};
 
 template<>
 struct BlobCurdRequestParam<BLOB_CURD_TYPE(fill_last_tensor)> final {
-  DEFINE_FIELD_AND_ACCESSOR(int64_t, offset);
+  POD_PROTO_DEFINE_FIELD(int64_t, mem_zone_id);
+  POD_PROTO_DEFINE_FIELD(int64_t, global_blob_id);
+  POD_PROTO_DEFINE_FIELD(int64_t, offset);
 };
 
 template<template <BlobCurdType> class BlobCurdParam>
 struct BlobCurdHeader {
-  DEFINE_FIELD_AND_ACCESSOR(int64_t, token);
-  DEFINE_FIELD_AND_ACCESSOR(int64_t, machine_id);
-  DEFINE_FIELD_AND_ACCESSOR(int64_t, mem_zone_id);
-  DEFINE_FIELD_AND_ACCESSOR(int64_t, global_blob_id);
-  DEFINE_FIELD_AND_ACCESSOR(int64_t, data_byte_size);
+  POD_PROTO_DEFINE_FIELD(int64_t, token);
+  POD_PROTO_DEFINE_FIELD(int64_t, machine_id);
+  POD_PROTO_DEFINE_FIELD(int64_t, data_byte_size);
 
   size_t total_byte_size() const { return sizeof(*this) + data_byte_size(); }
 
-  // a simulation to protobuf's oneof utils
-  // eg. 1) blob_curd_header.fill_last_tensor()
-  //     2) blob_curd_header->mutable_fill_last_tensor()
-  DEFINE_FIELD_AND_ACCESSOR(BlobCurdType, curd_type_case);
- public:  
-#define DEFINE_CURD_ONEOF_CASE(method)                                  \
-  const BlobCurdParam<BLOB_CURD_TYPE(method)>& method() const {  \
-    CHECK_EQ(curd_type_case(), BLOB_CURD_TYPE(method));        \
-    return method_;                                                     \
-  } \
-  BlobCurdParam<BLOB_CURD_TYPE(method)>* mutable_##method() {  \
-    set_curd_type_case(BLOB_CURD_TYPE(method)); \
-    return &method_;                                              \
-  }
-  OF_PP_FOR_EACH_TUPLE(DEFINE_CURD_ONEOF_CASE, BLOB_CURD_METHOD_SEQ);
-#undef DEFINE_CURD_ONEOF_CASE
-
- private:
-  union {
-#define DECLEAR_CURD_ONEOF_CASE(x) BlobCurdParam<BLOB_CURD_TYPE(x)> x##_;
-  OF_PP_FOR_EACH_TUPLE(DECLEAR_CURD_ONEOF_CASE, BLOB_CURD_METHOD_SEQ)
-#undef DECLEAR_CURD_ONEOF_CASE
-  };
+#define MAKE_ONEOF_CASE(method) POD_PROTO_ONEOF_CASE(BlobCurdParam<BLOB_CURD_TYPE(method)>, method)
+  POD_PROTO_DEFINE_FIELD(BlobCurdType, POD_PROTO_ONEOF_CASE(curd_type));
+  POD_PROTO_DEFINE_ONEOF_UNION(OF_PP_FOR_EACH_TUPLE(MAKE_ONEOF_CASE, BLOB_CURD_METHOD_SEQ));
+  POD_PROTO_DEFINE_ONEOF_ACCESSOR(BLOB_CURD_TYPE, curd_type,
+                                  OF_PP_FOR_EACH_TUPLE(MAKE_ONEOF_CASE, BLOB_CURD_METHOD_SEQ));
+#undef MAKE_ONEOF_CASE
 };
 
 using BlobCurdRequestHeader = BlobCurdHeader<BlobCurdRequestParam>;
@@ -86,9 +61,9 @@ struct BlobCurdResponse {
 };
 
 struct BlobCurdPackageHeader {
-  size_t package_byte_limit;
-  size_t items_byte_size;
-  size_t num_items;
+  POD_PROTO_DEFINE_FIELD(int64_t, package_byte_limit);
+  POD_PROTO_DEFINE_FIELD(int64_t, items_byte_size);
+  POD_PROTO_DEFINE_FIELD(int64_t, num_items);
 };
 
 template<typename RequestOrResponse>
--- oneflow/core/common/pod_proto.h
+++ oneflow/core/common/pod_proto.h
@@ -15,7 +15,7 @@
 
 #define POD_PROTO_DEFINE_ONEOF(oneof_name, type_and_field_name_seq)      \
   POD_PROTO_DEFINE_ONEOF_ENUM_TYPE(oneof_name, type_and_field_name_seq); \
-  POD_PROTO_DEFINE_ONEOF_ACCESSOR(oneof_name, type_and_field_name_seq)   \
+  POD_PROTO_DEFINE_ONEOF_ACCESSOR(POD_PROTO_ONEOF_ENUM_VALUE, oneof_name, type_and_field_name_seq) \
   POD_PROTO_DEFINE_ONEOF_UNION(type_and_field_name_seq);                 \
   POD_PROTO_DEFINE_FIELD(POD_PROTO_ONEOF_ENUM_TYPE(oneof_name), POD_PROTO_ONEOF_CASE(oneof_name));
 
@@ -27,21 +27,21 @@
     OF_PP_FOR_EACH_TUPLE(MAKE_POD_PROTO_ONEOF_ENUM_CASE, type_and_field_name_seq) \
   }
 
-#define MAKE_POD_PROTO_ONEOF_ENUM_CASE(type, field_name) POD_PROTO_ONEOF_ENUM_TYPE(field_name),
+#define MAKE_POD_PROTO_ONEOF_ENUM_CASE(type, field_name) POD_PROTO_ONEOF_ENUM_VALUE(field_name),
 
-#define POD_PROTO_DEFINE_ONEOF_ACCESSOR(oneof_name, type_and_field_name_seq)    \
-  OF_PP_SEQ_PRODUCT_FOR_EACH_TUPLE(MAKE_POD_PROTO_ONEOF_ACCESSOR, (oneof_name), \
+#define POD_PROTO_DEFINE_ONEOF_ACCESSOR(get_enum_value, oneof_name, type_and_field_name_seq)    \
+  OF_PP_SEQ_PRODUCT_FOR_EACH_TUPLE(MAKE_POD_PROTO_ONEOF_ACCESSOR, (get_enum_value), (oneof_name), \
                                    type_and_field_name_seq)
 
-#define MAKE_POD_PROTO_ONEOF_ACCESSOR(oneof_name, pair)                     \
+#define MAKE_POD_PROTO_ONEOF_ACCESSOR(get_enum_value, oneof_name, pair)                     \
   const OF_PP_PAIR_FIRST(pair) & OF_PP_PAIR_SECOND(pair)() const {          \
     CHECK(POD_PROTO_ONEOF_CASE(oneof_name)()                                \
-          == POD_PROTO_ONEOF_ENUM_VALUE(OF_PP_PAIR_SECOND(pair)));          \
+          == get_enum_value(OF_PP_PAIR_SECOND(pair)));          \
     return OF_PP_CAT(OF_PP_PAIR_SECOND(pair), _);                           \
   }                                                                         \
   OF_PP_PAIR_FIRST(pair) * OF_PP_CAT(mutable_, OF_PP_PAIR_SECOND(pair))() { \
     OF_PP_CAT(set_, POD_PROTO_ONEOF_CASE(oneof_name))                       \
-    (POD_PROTO_ONEOF_ENUM_VALUE(OF_PP_PAIR_SECOND(pair)));                  \
+    (get_enum_value(OF_PP_PAIR_SECOND(pair)));                  \
     return &OF_PP_CAT(OF_PP_PAIR_SECOND(pair), _);                          \
   }
 
