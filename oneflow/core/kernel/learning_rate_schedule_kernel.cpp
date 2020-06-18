17f0d95fee0a8dc61f5e489ae67dc737259ef31e
ShawnXuan <xiexuanx2@gmail.com>
2020-06-18 13:07:41 +0800

    fix lr
    Former-commit-id: 3c28ecd8eb40baeda3c2c3fb6b62b5f9caad3bd4

--- oneflow/core/kernel/learning_rate_schedule_kernel.cpp
+++ oneflow/core/kernel/learning_rate_schedule_kernel.cpp
@@ -51,6 +51,18 @@ bool TriggerWarmup(const LearningRateScheduleOpConf& conf, double lr, int64_t tr
   }
 }
 
+int64_t WarmupBatches(const LearningRateScheduleOpConf& conf) {
+  if (!conf.has_warmup_conf()) { return 0L; }
+  const WarmupConf& warmup_conf = conf.warmup_conf();
+  if (warmup_conf.has_constant_conf()) {
+    return warmup_conf.constant_conf().warmup_batches();
+  } else if (warmup_conf.has_linear_conf()) {
+    return warmup_conf.linear_conf().warmup_batches();
+  } else {
+    UNIMPLEMENTED();
+  }
+}
+
 double GetWarmupLearningRate(const WarmupConf& conf, double lr, int64_t train_step) {
   if (conf.has_constant_conf()) {
     return ConstantWarmupLearningRate(conf.constant_conf(), lr, train_step);
@@ -181,8 +193,10 @@ void LearningRateScheduleKernel::ForwardDataContent(
   if (TriggerWarmup(conf, learning_rate, train_step)) {
     learning_rate = GetWarmupLearningRate(conf.warmup_conf(), learning_rate, train_step);
   } else if (conf.has_learning_rate_decay()) {
-    learning_rate = GetDecayedLearningRate(conf.learning_rate_decay(), learning_rate, train_step);
+    learning_rate = GetDecayedLearningRate(conf.learning_rate_decay(), learning_rate, 
+                                           train_step - WarmupBatches(conf));
   }
+  LOG(INFO) << "learning_rate " << learning_rate;
   *BnInOp2Blob("out")->mut_dptr<float>() = learning_rate;
 }
 
