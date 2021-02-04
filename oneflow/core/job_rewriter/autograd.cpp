d84f8b581c03f086f9954cd8a75f79fdd8180507
liujuncheng <liujuncheng1022@gmail.com>
2021-02-04 10:23:49 +0800

    fix
    Former-commit-id: 0f77f149b1adc005863af9dad07ba8d8023255d7

--- oneflow/core/job_rewriter/autograd.cpp
+++ oneflow/core/job_rewriter/autograd.cpp
@@ -947,11 +947,12 @@ void AddDiffParallelCast(const OpGraph& op_graph, JobBuilder* job_builder,
             .Op("hierarchical_parallel_cast")
             .Input("in", GenLogicalBlobName(diff_lbi))
             .Output("out")
-            .Attr("parallel_hierarchy", *model_op_node->parallel_hierarchy())
-            .Attr("parallel_distribution", parallel_distribution)
-            .Attr("grad_mode", "auto")
-            .Attr("grad_parallel_hierarchy", Shape())
-            .Attr("grad_parallel_distribution", std::vector<std::string>())
+            .Attr<Shape>("parallel_hierarchy", *model_op_node->parallel_hierarchy())
+            .Attr<std::vector<std::string>>("parallel_distribution", parallel_distribution)
+            .Attr<std::string>("grad_mode", "auto")
+            .Attr<Shape>("grad_parallel_hierarchy", Shape())
+            .Attr<std::vector<std::string>>("grad_parallel_distribution",
+                                            std::vector<std::string>())
             .ScopeSymbolId(scope_symbol_id)
             .Build();
     job_builder->AddOps(model_op_node->parallel_desc().parallel_conf(),
