a61fb27dbd880ab96d5a7f5487b3067279e95e23
guo-ran <360112263@qq.com>
2021-02-04 12:07:29 +0800

    test case
    Former-commit-id: 4560ecc6ca3ac76b64f18b7a695a2fd2dc695a16

---
+++ oneflow/python/test/ops/test_hierarchical_parallel_cast1.py
@@ -0,0 +1,102 @@
+"""
+Copyright 2020 The OneFlow Authors. All rights reserved.
+
+Licensed under the Apache License, Version 2.0 (the "License");
+you may not use this file except in compliance with the License.
+You may obtain a copy of the License at
+
+    http://www.apache.org/licenses/LICENSE-2.0
+
+Unless required by applicable law or agreed to in writing, software
+distributed under the License is distributed on an "AS IS" BASIS,
+WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
+See the License for the specific language governing permissions and
+limitations under the License.
+"""
+import unittest
+import numpy as np
+import oneflow as flow
+
+
+# test 1D
+def _test(test_case):
+    flow.clear_default_session()
+    flow.config.gpu_device_num(4)
+    func_config = flow.FunctionConfig()
+    func_config.default_data_type(flow.float32)
+
+    @flow.global_function("predict", function_config=func_config)
+    def test_fn(x: flow.typing.Numpy.Placeholder((1024, 1024)),) -> flow.typing.Numpy:
+        x = flow.hierarchical_parallel_cast(
+            x, parallel_hierarchy=[2, 2], parallel_distribution=["B", "B"],
+        )
+        v = flow.get_variable(
+            name="v",
+            shape=(1024, 1024),
+            parallel_hierarchy=(2, 2),
+            parallel_distribution=["S(1)", "S(1)"],
+            initializer=flow.ones_initializer(),
+        )
+        x = flow.matmul(x, v)
+        x = flow.math.relu(x)
+        x = flow.hierarchical_parallel_cast(
+            x, parallel_hierarchy=[4], parallel_distribution=["S(0)"]
+        )
+        x = flow.math.relu(x)
+        x = flow.hierarchical_parallel_cast(
+            x, parallel_hierarchy=[4], parallel_distribution=["B"]
+        )
+        return x
+
+    x_arr = np.random.rand(1024, 1024).astype(np.float32)
+    y_arr = test_fn(x_arr)
+    print("y_arr", y_arr.shape, y_arr.flatten()[0:10])
+    print("x_arr", x_arr.shape, x_arr.flatten()[0:10])
+    test_case.assertTrue(np.allclose(y_arr.flatten(), x_arr.flatten()))
+
+
+def _test_train(test_case):
+    flow.clear_default_session()
+    flow.config.gpu_device_num(4)
+    func_config = flow.FunctionConfig()
+    func_config.default_data_type(flow.float32)
+
+    @flow.global_function("train", function_config=func_config)
+    def test_fn(x: flow.typing.Numpy.Placeholder((1024, 1024)),) -> flow.typing.Numpy:
+        x = flow.hierarchical_parallel_cast(
+            x, parallel_hierarchy=[2, 2], parallel_distribution=["B", "B"],
+        )
+        v = flow.get_variable(
+            name="v",
+            shape=(1024, 1024),
+            parallel_hierarchy=(2, 2),
+            parallel_distribution=["S(1)", "S(1)"],
+            initializer=flow.ones_initializer(),
+        )
+        x = flow.matmul(x, v)
+        x = flow.math.relu(x)
+        x = flow.hierarchical_parallel_cast(
+            x, parallel_hierarchy=[4], parallel_distribution=["S(1)"]
+        )
+        x = flow.math.relu(x)
+        flow.optimizer.SGD(
+            flow.optimizer.PiecewiseConstantScheduler([], [1e-3]), momentum=0
+        ).minimize(x)
+        return x
+
+    x_arr = np.random.rand(1024, 1024).astype(np.float32)
+    y_arr = test_fn(x_arr)
+    print("y_arr", y_arr.shape, y_arr.flatten()[0:10])
+    print("x_arr", x_arr.shape, x_arr.flatten()[0:10])
+    test_case.assertTrue(np.allclose(y_arr.flatten(), x_arr.flatten()))
+
+
+@flow.unittest.skip_unless_1n4d()
+class TestHierarchicalParallelCast(flow.unittest.TestCase):
+    def test_hierarchy_parallel_cast(test_case):
+        _test(test_case)
+        #_test_train(test_case)
+
+
+if __name__ == "__main__":
+    unittest.main()
