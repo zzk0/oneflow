bef8907a062e714c5b27dbad3961817356410116
guo-ran <360112263@qq.com>
2021-03-13 11:19:24 +0800

    reduce parallel_desc
    Former-commit-id: 4b63d77cbbac089e54436198e80a9aa8a4003456

--- oneflow/core/graph/boxing/hierarchical_sub_task_graph_builder.cpp
+++ oneflow/core/graph/boxing/hierarchical_sub_task_graph_builder.cpp
@@ -36,88 +36,98 @@ limitations under the License.
 namespace oneflow {
 namespace {
 
-void ParallelhierarchyReduce(const Shape& parallel_hierarchy,
-                             const ParallelDistribution& parallel_distribution,
-                             Shape* return_parallel_hierarchy,
-                             ParallelDistribution* return_parallel_distribution) {
-  DimVector reduced_parallel_hierarchy;
-  reduced_parallel_hierarchy.push_back(parallel_hierarchy.At(0));
+void ParallelAxesReduce(const ParallelDesc& parallel_desc,
+                        const ParallelDistribution& parallel_distribution,
+                        ParallelDesc* return_parallel_desc,
+                        ParallelDistribution* return_parallel_distribution) {
+  const auto& hierarchy = parallel_desc.hierarchy();
+  DimVector reduced_hierarchy;
+  reduced_hierarchy.push_back(hierarchy->At(0));
   *return_parallel_distribution->add_sbp_parallel() = parallel_distribution.sbp_parallel(0);
-  FOR_RANGE(int64_t, i, 1, parallel_hierarchy.NumAxes()) {
+  FOR_RANGE(int64_t, i, 1, hierarchy->NumAxes()) {
     if (parallel_distribution.sbp_parallel(i) == parallel_distribution.sbp_parallel(i - 1)) {
-      // contiguous, can reduce
-      reduced_parallel_hierarchy.back() *= parallel_hierarchy.At(i);
+      reduced_hierarchy.back() *= hierarchy->At(i);
     } else {
-      reduced_parallel_hierarchy.push_back(parallel_hierarchy.At(i));
+      reduced_hierarchy.push_back(hierarchy->At(i));
       *return_parallel_distribution->add_sbp_parallel() = parallel_distribution.sbp_parallel(i);
     }
   }
-  *return_parallel_hierarchy = Shape(reduced_parallel_hierarchy);
+  ParallelConf reduced_parallel_conf = parallel_desc.parallel_conf();
+  Shape(reduced_hierarchy).ToProto(reduced_parallel_conf.mutable_hierarchy());
+  *return_parallel_desc = ParallelDesc(reduced_parallel_conf);
 }
 
-void CollaborativeParallelhierarchyReduce(
-    const Shape& src_parallel_hierarchy, const ParallelDistribution& src_parallel_distribution,
-    const Shape& dst_parallel_hierarchy, const ParallelDistribution& dst_parallel_distribution,
-    Shape* src_return_parallel_hierarchy, ParallelDistribution* src_return_parallel_distribution,
-    Shape* dst_return_parallel_hierarchy, ParallelDistribution* dst_return_parallel_distribution) {
-  CHECK_EQ(src_parallel_hierarchy.NumAxes(), dst_parallel_hierarchy.NumAxes());
-  DimVector src_reduced_parallel_hierarchy;
-  src_reduced_parallel_hierarchy.push_back(src_parallel_hierarchy.At(0));
-  *src_return_parallel_distribution->add_sbp_parallel() = src_parallel_distribution.sbp_parallel(0);
+void CollaborativeParallelAxesReduce(const ParallelDesc& in_parallel_desc,
+                                     const ParallelDistribution& in_parallel_distribution,
+                                     const ParallelDesc& out_parallel_desc,
+                                     const ParallelDistribution& out_parallel_distribution,
+                                     ParallelDesc* in_return_parallel_desc,
+                                     ParallelDistribution* in_return_parallel_distribution,
+                                     ParallelDesc* out_return_parallel_desc,
+                                     ParallelDistribution* out_return_parallel_distribution) {
+  const auto& in_hierarchy = in_parallel_desc.hierarchy();
+  const auto& out_hierarchy = out_parallel_desc.hierarchy();
+  CHECK_EQ(in_hierarchy->NumAxes(), out_hierarchy->NumAxes());
 
-  DimVector dst_reduced_parallel_hierarchy;
-  dst_reduced_parallel_hierarchy.push_back(dst_parallel_hierarchy.At(0));
-  *dst_return_parallel_distribution->add_sbp_parallel() = dst_parallel_distribution.sbp_parallel(0);
+  DimVector in_reduced_hierarchy;
+  in_reduced_hierarchy.push_back(in_hierarchy->At(0));
+  *in_return_parallel_distribution->add_sbp_parallel() = in_parallel_distribution.sbp_parallel(0);
 
-  FOR_RANGE(int64_t, i, 1, src_parallel_hierarchy.NumAxes()) {
-    if ((src_parallel_distribution.sbp_parallel(i) == src_parallel_distribution.sbp_parallel(i - 1))
-        && (dst_parallel_distribution.sbp_parallel(i)
-            == dst_parallel_distribution.sbp_parallel(i - 1))) {
-      // contiguous, can reduce
-      src_reduced_parallel_hierarchy.back() *= src_parallel_hierarchy.At(i);
-      dst_reduced_parallel_hierarchy.back() *= dst_parallel_hierarchy.At(i);
+  DimVector out_reduced_hierarchy;
+  out_reduced_hierarchy.push_back(out_hierarchy->At(0));
+  *out_return_parallel_distribution->add_sbp_parallel() = out_parallel_distribution.sbp_parallel(0);
+
+  FOR_RANGE(int64_t, i, 1, in_hierarchy->NumAxes()) {
+    if ((in_parallel_distribution.sbp_parallel(i) == in_parallel_distribution.sbp_parallel(i - 1))
+        && (out_parallel_distribution.sbp_parallel(i)
+            == out_parallel_distribution.sbp_parallel(i - 1))) {
+      in_reduced_hierarchy.back() *= in_hierarchy->At(i);
+      out_reduced_hierarchy.back() *= out_hierarchy->At(i);
     } else {
-      src_reduced_parallel_hierarchy.push_back(src_parallel_hierarchy.At(i));
-      *src_return_parallel_distribution->add_sbp_parallel() =
-          src_parallel_distribution.sbp_parallel(i);
+      in_reduced_hierarchy.push_back(in_hierarchy->At(i));
+      *in_return_parallel_distribution->add_sbp_parallel() =
+          in_parallel_distribution.sbp_parallel(i);
 
-      dst_reduced_parallel_hierarchy.push_back(dst_parallel_hierarchy.At(i));
-      *dst_return_parallel_distribution->add_sbp_parallel() =
-          dst_parallel_distribution.sbp_parallel(i);
+      out_reduced_hierarchy.push_back(out_hierarchy->At(i));
+      *out_return_parallel_distribution->add_sbp_parallel() =
+          out_parallel_distribution.sbp_parallel(i);
     }
   }
-  *src_return_parallel_hierarchy = Shape(src_reduced_parallel_hierarchy);
-  *dst_return_parallel_hierarchy = Shape(dst_reduced_parallel_hierarchy);
+
+  ParallelConf in_reduced_parallel_conf = in_parallel_desc.parallel_conf();
+  Shape(in_reduced_hierarchy).ToProto(in_reduced_parallel_conf.mutable_hierarchy());
+  *in_return_parallel_desc = ParallelDesc(in_reduced_parallel_conf);
+
+  ParallelConf out_reduced_parallel_conf = out_parallel_desc.parallel_conf();
+  Shape(out_reduced_hierarchy).ToProto(out_reduced_parallel_conf.mutable_hierarchy());
+  *out_return_parallel_desc = ParallelDesc(out_reduced_parallel_conf);
 }
 
-void InOutParallelhierarchyReduce(const Shape& in_parallel_hierarchy,
-                                  const Shape& out_parallel_hierarchy,
-                                  const ParallelDistribution& in_parallel_distribution,
-                                  const ParallelDistribution& out_parallel_distribution,
-                                  Shape* return_in_parallel_hierarchy,
-                                  Shape* return_out_parallel_hierarchy,
-                                  ParallelDistribution* return_in_parallel_distribution,
-                                  ParallelDistribution* return_out_parallel_distribution) {
-  if (in_parallel_hierarchy.NumAxes() == 1 && out_parallel_hierarchy.NumAxes() == 1) {
-    LOG(INFO) << "1D hierarchy";
-    *return_in_parallel_distribution->add_sbp_parallel() = in_parallel_distribution.sbp_parallel(0);
-    *return_out_parallel_distribution->add_sbp_parallel() =
-        out_parallel_distribution.sbp_parallel(0);
-    *return_in_parallel_hierarchy = Shape(in_parallel_hierarchy);
-    *return_out_parallel_hierarchy = Shape(out_parallel_hierarchy);
-  } else if (in_parallel_hierarchy.NumAxes() != out_parallel_hierarchy.NumAxes()) {
-    LOG(INFO) << "ParallelhierarchyReduce";
-    ParallelhierarchyReduce(in_parallel_hierarchy, in_parallel_distribution,
-                            return_in_parallel_hierarchy, return_in_parallel_distribution);
-    ParallelhierarchyReduce(out_parallel_hierarchy, out_parallel_distribution,
-                            return_out_parallel_hierarchy, return_out_parallel_distribution);
+void InOutParallelAxesReduce(const ParallelDesc& in_parallel_desc,
+                             const ParallelDesc& out_parallel_desc,
+                             const ParallelDistribution& in_parallel_distribution,
+                             const ParallelDistribution& out_parallel_distribution,
+                             ParallelDesc* return_in_parallel_desc,
+                             ParallelDesc* return_out_parallel_desc,
+                             ParallelDistribution* return_in_parallel_distribution,
+                             ParallelDistribution* return_out_parallel_distribution) {
+  const int64_t in_hierarchy_axes = in_parallel_desc.hierarchy()->NumAxes();
+  const int64_t out_hierarchy_axes = out_parallel_desc.hierarchy()->NumAxes();
+  if (in_hierarchy_axes == 1 && out_hierarchy_axes == 1) {
+    *return_in_parallel_desc = in_parallel_desc;
+    *return_out_parallel_desc = out_parallel_desc;
+    *return_in_parallel_distribution = in_parallel_distribution;
+    *return_out_parallel_distribution = out_parallel_distribution;
+  } else if (in_hierarchy_axes != out_hierarchy_axes) {
+    ParallelAxesReduce(in_parallel_desc, in_parallel_distribution, return_in_parallel_desc,
+                       return_in_parallel_distribution);
+    ParallelAxesReduce(out_parallel_desc, out_parallel_distribution, return_out_parallel_desc,
+                       return_out_parallel_distribution);
   } else {
-    LOG(INFO) << "CollaborativeParallelhierarchyReduce";
-    // reduce when both src and dst can reduce.
-    CollaborativeParallelhierarchyReduce(
-        in_parallel_hierarchy, in_parallel_distribution, out_parallel_hierarchy,
-        out_parallel_distribution, return_in_parallel_hierarchy, return_in_parallel_distribution,
-        return_out_parallel_hierarchy, return_out_parallel_distribution);
+    CollaborativeParallelAxesReduce(in_parallel_desc, in_parallel_distribution, out_parallel_desc,
+                                    out_parallel_distribution, return_in_parallel_desc,
+                                    return_in_parallel_distribution, return_out_parallel_desc,
+                                    return_out_parallel_distribution);
   }
 }
 
@@ -648,16 +658,17 @@ Maybe<SubTskGphBuilderStatus> HierarchicalSubTskGphBuilder::Build(
     const ParallelDesc& out_parallel_desc, const LogicalBlobId& lbi,
     const BlobDesc& logical_blob_desc, const ParallelDistribution& in_parallel_distribution,
     const ParallelDistribution& out_parallel_distribution, const Shape& time_shape) const {
-  const Shape& in_parallel_hierarchy = *in_parallel_desc.hierarchy();
-  const Shape& out_parallel_hierarchy = *out_parallel_desc.hierarchy();
-  Shape reduced_in_parallel_hierarchy;
-  Shape reduced_out_parallel_hierarchy;
+  ParallelDesc reduced_in_parallel_desc = in_parallel_desc;
+  ParallelDesc reduced_out_parallel_desc = out_parallel_desc;
   ParallelDistribution reduced_in_parallel_distribution;
   ParallelDistribution reduced_out_parallel_distribution;
-  InOutParallelhierarchyReduce(
-      in_parallel_hierarchy, out_parallel_hierarchy, in_parallel_distribution,
-      out_parallel_distribution, &reduced_in_parallel_hierarchy, &reduced_out_parallel_hierarchy,
-      &reduced_in_parallel_distribution, &reduced_out_parallel_distribution);
+  InOutParallelAxesReduce(in_parallel_desc, out_parallel_desc, in_parallel_distribution,
+                          out_parallel_distribution, &reduced_in_parallel_desc,
+                          &reduced_out_parallel_desc, &reduced_in_parallel_distribution,
+                          &reduced_out_parallel_distribution);
+  const Shape& reduced_in_parallel_hierarchy = *reduced_in_parallel_desc.hierarchy();
+  const Shape& reduced_out_parallel_hierarchy = *reduced_out_parallel_desc.hierarchy();
+
   CHECK_LE_OR_RETURN(reduced_in_parallel_hierarchy.NumAxes(), 2);
   CHECK_LE_OR_RETURN(reduced_out_parallel_hierarchy.NumAxes(), 2);
 
